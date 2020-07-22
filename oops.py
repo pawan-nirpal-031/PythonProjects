eac650f15aabc85af0674d90462c8e2b6276b77b
pawan-nirpal-o31 <pawannirpal@gmail.com>
2020-07-22 11:02:47 +0530

    wrap week 1 databases

--- oops.py
+++ oops.py
@@ -1,9 +1,32 @@
 class animal:
-    age =0
-    food ="none"
-    def make_noise(self):
-        print('bark')
+    xpos =0
+    ypos =0
+    name =""
+    def __init__(self,nm="none"):
+        print('hello I am ',nm)
+        self.name = nm
+    def __del(self):
+        print("time to say goodbye", self.name)
+    def noise(self,n):
+        print("z....",n)
+    def move(self,x,y):
+        self.xpos = x
+        self.ypos = y
+        print('walking happily ',self.xpos," , ",self.ypos)
 
-an = animal()
-an.make_noise()
-print(dir(an))
+class dog(animal):
+    def __init__(self,ji):
+        print("lets bark!",ji)
+        animal.name = ji
+    animal("dory").move(11,2)
+    def myn(self):
+        print('hello ',self.name)
+
+
+a1 = animal("boog")
+a2 = animal("sory")
+a1.move(1,2)
+a2.move(3,4)
+a1.noise("bark")
+a2.noise("doek")
+dog('bog').myn()
