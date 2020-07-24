6cebd0341ba51d84ab28987f3f230620be8aeaca
pawan-nirpal-o31 <pawannirpal@gmail.com>
2020-07-24 18:59:26 +0530

    firstWebcrawler

---
+++ webScraper.py
@@ -0,0 +1,12 @@
+import urllib.request as ureq,urllib.parse as upar,urllib.error as uerr 
+from bs4 import BeautifulSoup
+
+try:
+    url = 'http://'+input('Enter website ')
+    html = ureq.urlopen(url).read()
+    soup = BeautifulSoup(html,'html.parser')
+    tags = soup('a')
+    for tg in tags:
+        print(tg.get('href',None))
+except Exception:
+    print('Action prohibited \n:-(','Quitting this request')
