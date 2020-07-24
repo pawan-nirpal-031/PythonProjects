10beedbc677ba30f7cdc93fe23cee2c05f09b58e
pawan-nirpal-o31 <pawannirpal@gmail.com>
2020-07-24 19:26:44 +0530

    webScraping done

--- webScraper.py
+++ webScraper.py
@@ -2,11 +2,14 @@ import urllib.request as ureq,urllib.parse as upar,urllib.error as uerr
 from bs4 import BeautifulSoup
 
 try:
-    url = 'http://'+input('Enter website ')
+    url = 'http://'+input('Enter website : ')
     html = ureq.urlopen(url).read()
     soup = BeautifulSoup(html,'html.parser')
     tags = soup('a')
+    fileh = open('linksInGoogle.txt','w')
+    print(soup)
     for tg in tags:
         print(tg.get('href',None))
+        
 except Exception:
     print('Action prohibited \n:-(','Quitting this request')
