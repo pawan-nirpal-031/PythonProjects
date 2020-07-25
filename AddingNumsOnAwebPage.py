4a5bba39439cb50dd43e06e35607b19eda63b172
pawan-nirpal-o31 <pawannirpal@gmail.com>
2020-07-25 10:47:48 +0530

    python review

--- AddingNumsOnAwebPage.py
+++ AddingNumsOnAwebPage.py
@@ -1,7 +1,8 @@
 import urllib.request as ureq,urllib.parse as upar,urllib.error as uerr 
 from bs4 import BeautifulSoup
-
+import time
 try:
+    t1 = time.time()
     url = input("Enter url :")
     html = ureq.urlopen(url).read()
     sp = BeautifulSoup(html,'html.parser')
@@ -10,6 +11,8 @@ try:
     for t in tags:
         sum+=int(t.contents[0])
     print(sum)
+    t2 = time.time()
+    print(str(t2-t1))
 except Exception:
     print('Unable to do it')
 
---
+++ check.py
@@ -0,0 +1,14 @@
+# def add(a,b,c='None'):
+#     if(c=='None'):
+#         return a+b
+#     else:
+#         return a+b+c
+
+# x = add
+# print(x(1,2))
+name = 'christoper hidwig harry potter'.split()
+print(name)
+if('chris' in name[0]):
+    print('yes')
+else:
+    print('no')
